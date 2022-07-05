8540392732048c62ceb94ffe8e1dd6ca23f6dc0b
MinaKrsticc <mina.krstic@syrmia.com>
2022-07-05 13:46:03 +0200

    with constructor and destructor

--- my_string.cpp
+++ my_string.cpp
@@ -1,17 +1,68 @@
-namespace my_string {
+#include <iostream>
+#include "my_string.h"
+#include <string.h>
+using namespace std;
 
-class MyString{
-public:
-    MyString(){};
-    MyString(const MyString&){};//konstruktor kopije
-    MyString(char*){};
-    ~MyString(){};
-};
-
-MyString CopyString(MyString elementToBeCoped)
+namespace my_string
 {
-    MyString elementInWhichCoped = elementToBeCoped;
-    return elementInWhichCoped;
+
+MyString::MyString()
+{
+    this->stringValue = nullptr;
 }
 
+MyString::~MyString()
+{
+    if(this->stringValue == nullptr)
+        delete this->stringValue;
+}
+
+MyString::MyString(char* newString)
+{
+    int lenght = strlen(newString);
+    this->stringValue = new char[lenght];
+    strcpy(this->stringValue, newString);
+}
+
+MyString::MyString(const MyString& copy)
+{
+    int lenght = strlen(copy.stringValue);
+    this->stringValue = new char[lenght];
+    strcpy(this->stringValue, copy.stringValue);
+}
+
+ostream& MyString::operator<<(ostream& out)
+{
+    out << this->stringValue ;
+    return out;
+}
+
+istream& MyString::operator>>(istream& in)
+{
+    cout << "Please enter the numerator part: ";
+    in >> this->stringValue;
+    return in;
+}
+
+bool MyString::operator==(const MyString& rhs)
+{
+    return this->stringValue == rhs.stringValue;
+}
+
+MyString MyString::operator+(const MyString& rhs)
+{
+    rhs.stringValue = this->stringValue + rhs.stringValue ;
+    return rhs.stringValue;
+
+}
+
+char* MyString::getStringValue()
+{
+    return this->stringValue;
+}
+
+void MyString::setStringValue(char* newStringValue)
+{
+    this->stringValue = newStringValue;
+}
 };
--- my_string.h
+++ my_string.h
@@ -1,4 +1,27 @@
 #ifndef MY_STRING_H
 #define MY_STRING_H
+#include <iostream>
+using namespace std;
+
+namespace my_string {
+
+class MyString{
+private:
+    char* stringValue;
+public:
+    MyString();
+    MyString(const MyString&);//konstruktor kopije
+    MyString(char* newString);
+    ~MyString();//destruktor
+
+    char* getStringValue();
+    void setStringValue(char* newStringValue);
+
+    istream& operator>>(istream& in);
+    ostream& operator<<(ostream& out);
+    bool operator==(const MyString& rhs);
+    MyString operator+ (const MyString& rhs);
+};
+}
 
 #endif // MY_STRING_H
